8c42b5a64efb1e70853befdf41acd7abff9b885c
Jakob Lange <jakob.lange@bring.com>
2022-08-05 11:16:51 +0200

    setup routes for 'books' and 'quick thoughts'

--- server.py
+++ server.py
@@ -10,5 +10,13 @@ app = Flask(__name__)
 def home():
     return render_template("index.html", fa_kit=os.environ.get("FA_KIT"))
 
+@app.route("/books")
+def get_books():
+    return "Welcome to the books page!"
+
+@app.route("/quick_thoughts")
+def get_quick_thoughts():
+    return "Welcome to the quick thoughts page!"
+
 if __name__ == "__main__":
     app.run(debug=True)
