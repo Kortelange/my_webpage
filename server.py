925e8dcc84b95b1af0da3f95b86443f078f3f365
Jakob Lange <jakob.lange@bring.com>
2022-08-05 10:44:56 +0200

    add basic styling to index.html

--- server.py
+++ server.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", fa_kit=os.environ.get("FA_KIT"))
 
 if __name__ == "__main__":
     app.run(debug=True)
