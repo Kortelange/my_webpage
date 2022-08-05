4386e2736491cd3cda163b9ff8a7b0ce3d303735
Jakob Lange <jakob.lange@bring.com>
2022-08-05 21:00:46 +0200

    read quick_thoughts from database

--- server.py
+++ server.py
@@ -2,10 +2,30 @@ from flask import Flask, render_template
 from dotenv import load_dotenv
 import os
 from fake_posts import test_posts
+from flask_sqlalchemy import SQLAlchemy
+import sqlite3
 
 load_dotenv()
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///my_webpage.db"
+db = SQLAlchemy(app)
+
+class QuickThought(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), unique=True, nullable=False)
+    content = db.Column(db.Text, nullable=False)
+
+db.create_all()
+
+
+# populate database
+# for thought in test_posts['thoughts']:
+#    db.session.add(
+#        QuickThought(title=thought['title'], content=thought['thought'])
+#    )
+#    db.session.commit()
+
 
 def get_book_from_id(id):
     for book in test_posts['books']:
@@ -30,8 +50,9 @@ def get_book(id):
 
 @app.route("/quick_thoughts")
 def get_quick_thoughts():
-    thoughts = test_posts['thoughts']
+    thoughts = QuickThought.query.all()
     return render_template("quick_thoughts.html", thoughts=thoughts, title="Quick Thoughts")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
