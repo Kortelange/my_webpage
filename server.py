447f82d63f9e7c40d3ca3bf5b5eeaa6f23dd8d65
Jakob Lange <jakob.lange@bring.com>
2022-08-06 09:33:33 +0200

    add post route for quick_thoughts

--- server.py
+++ server.py
@@ -1,14 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 from dotenv import load_dotenv
 import os
 from fake_posts import test_posts
 from flask_sqlalchemy import SQLAlchemy
-import sqlite3
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
 
 load_dotenv()
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///my_webpage.db"
+
+app.secret_key = "very secret"
 db = SQLAlchemy(app)
 
 class QuickThought(db.Model):
@@ -25,6 +30,12 @@ db.create_all()
 #        QuickThought(title=thought['title'], content=thought['thought'])
 #    )
 #    db.session.commit()
+
+
+class QuickThoughtForm(FlaskForm):
+    title = StringField('title', validators=[DataRequired()])
+    content = StringField('content', validators=[DataRequired()])
+    submit = SubmitField(label="Add thought")
 
 
 def get_book_from_id(id):
@@ -48,10 +59,30 @@ def get_book(id):
     return render_template('book.html', book=book, title=book['title'])
 
 
+@app.route("/books/new")
+def new_book():
+    return "hello"
+
+
 @app.route("/quick_thoughts")
 def get_quick_thoughts():
     thoughts = QuickThought.query.all()
     return render_template("quick_thoughts.html", thoughts=thoughts, title="Quick Thoughts")
+
+
+@app.route("/quick_thoughts/new", methods=["GET", "POST"])
+def new_quick_thought():
+    form = QuickThoughtForm()
+    if form.validate_on_submit():
+        db.session.add(
+            QuickThought(
+                title=form.title.data,
+                content=form.content.data
+            )
+        )
+        db.session.commit()
+        return redirect("/quick_thoughts")
+    return render_template("new_quick_thought.html", form=form, title="New Thought")
 
 
 if __name__ == "__main__":
